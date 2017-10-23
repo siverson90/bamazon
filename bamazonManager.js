6a6adba8cf9f323f6390be9c916f26331feb2e61
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-10-22 17:04:04 -0700

    completing logic for adding a new product

--- bamazonManager.js
+++ bamazonManager.js
@@ -46,12 +46,11 @@ function renderManagerQs(){
           break;
         case 'Add New Product':
           console.log("the user wants to" + response.managerDisplay);
-          // run through entire process of adding item
+          addNewItem();
           break;
         default:
           console.log('error on the case/switch');
       }
-
   });
 }
 
@@ -65,21 +64,26 @@ function renderManagerQs(){
  function renderTable(obj) {
   this.table = new Table({
     head: ['item_id', 'product_name', 'department_name', 'price','stock_quantity' ]
-  , colWidths: [10, 20, 20, 10, 17]
-  });
-
+    , colWidths: [10, 20, 20, 10, 17]
+    });
   var newArray=[];
     for(var i = 0; i < obj.length; i++) {
       var newItemArray = [];
-      newItemArray.push(obj[i].item_id, obj[i].product_name, obj[i].department_name, obj[i].price, obj[i].stock_quantity)
+      newItemArray.push(
+        obj[i].item_id, 
+        obj[i].product_name, 
+        obj[i].department_name, 
+        obj[i].price, 
+        obj[i].stock_quantity
+        )
       newArray.push(newItemArray);
       table.push(newArray[i]);
     }; 
     var showTable = table.toString();
 
     // var msg = clc.xterm(163).bgXterm(253);
-  console.log(showTable);
-}
+    console.log(showTable);
+  }
 
 function getLowInventory() {
   var lowInventoryThreshold = 5;
@@ -150,4 +154,55 @@ function addInventory() {
       });
     });
   });
- }
+}
+
+function addNewItem() {
+  
+  connection.query("SELECT department_name FROM products GROUP BY department_name", function(err,res) {
+    if (err) throw err;  
+
+    var promptArray = [];
+    for (var i = 0; i < res.length; i++) {
+      promptArray.push(res[i].department_name);
+    }
+    console.log(promptArray);
+
+    inquirer.prompt([
+    {
+      type: "input",
+      name: "product_name",
+      message: "What is the name of the product you'd like to add?"
+    },
+    {
+      type: 'list',
+      message: "What department does this product belong to?",
+      name: "department_name",
+      choices: promptArray
+    },
+    {
+      type: "input",
+      message: "What is the price of the product?",
+      name: "price"
+    },
+    {
+      type: "input",
+      message: "What is amount of inventory you'd like to list",
+      name: "stock_quantity"
+    },
+    ])
+    .then(function(response) {
+      var query = ("INSERT INTO products SET ?")
+      connection.query(query,[
+        {
+          product_name: response.product_name,
+          department_name: response.department_name,
+          price: response.price,
+          stock_quantity: response.stock_quantity
+        }
+        ],function(err,response) {
+        if (err) throw err;
+        console.log(response.affectedRows + " product inserted!\n");
+      });
+    });
+  });
+}
